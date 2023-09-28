f78c19747b418be60d37e31de3aedb5a8cc90f7a
Fredrickmureti <fredrickmureti612@gmail.com>
2023-09-27 19:49:53 -0700

    debugged

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -10,21 +10,28 @@
 int wildcmp(char *s1, char *s2)
 {
 	/* If both strings are empty, they are considered identical */
-	if (*s1 == '\0' && *s2 == '\0')
+	if (*s1 == '\0' && (*s2 == '\0' || (*s2 == '*' && *(s2 + 1) == '\0')))
 	{
 		return (1);
 	}
+
 	/* If the current characters match or s2 has a wildcard '*', continue comparing */
 	if (*s1 == *s2 || *s2 == '*')
 	{
 		/* Check if s2's '*' can replace any string, including an empty string */
-		if (*s2 == '*' && (*(s2 + 1) == '\0' || wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1)))
+		if (*s2 == '*')
 		{
-			return (1);
+			if (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1))
+			{
+				return (1);
+			}
 		}
+
 		/* If not, continue comparing characters */
 		return (wildcmp(s1 + 1, s2 + 1));
 	}
+
 	/* If the current characters don't match, and there's no wildcard '*', return 0 */
 	return (0);
 }
+
