72093d42155a19f45a8c1cb95542b554ca554067
Fredrickmureti <fredrickmureti612@gmail.com>
2023-10-04 17:59:41 -0700

    rectified

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
-    int i;
+    int num1, num2, result;
 
-    for (i = 0; i < argc; i++)
+    if (argc != 3)
     {
-        printf("%s\n", argv[i]);
+        printf("Error\n");
+        return 1;
     }
+
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[2]);
+
+    result = num1 * num2;
+
+    printf("%d\n", result);
 
     return 0;
 }
