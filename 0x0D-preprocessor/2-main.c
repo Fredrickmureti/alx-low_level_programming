2edfee675826bf65be3a448adc22c346e9cbf0f5
Fredrickmureti <fredrickmureti612@gmail.com>
2023-10-10 17:40:53 -0700

    task4

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void) {
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the current source file.
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
     printf("%s\n", __FILE__);
     return (0);
 }
